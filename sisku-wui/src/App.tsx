053c96633148d0d2b7eb747bbe92a664aa55a092
Yuya Kono <takohati0821@gmail.com>
2021-11-09 20:58:13 +0900

    refactoring

--- sisku-wui/src/App.tsx
+++ sisku-wui/src/App.tsx
@@ -1,28 +1,19 @@
-import React, { useEffect } from 'react';
 import './App.css';
+import React, { useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Controller, useForm } from 'react-hook-form';
 import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Input from '@mui/material/Input';
+import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import {Element, Hover, HoverResult} from './lsif';
+import { Element, Hover, HoverResult } from './lsif';
 
 type SearchResult = {
   hover: HoverResult;
   definition: Element;
   defRanges: Element[];
   moniker: Element;
-};
-
-const SearchBar = ({ onSearchTermChange }: { onSearchTermChange: (searchTerm: string) => void }) => {
-  return <TextField label="Search" onChange={(e) => onSearchTermChange(e.target.value)} />
-};
-
-const SearchResults = ({ searchResults }: { searchResults: SearchResult[] }) => {
-  return <div>
-    {searchResults.map((searchResult, i) =>
-      <Entry key={i} index={i} searchResult={searchResult} />
-    )}
-  </div>
 };
 
 // Get markdown string from Hover.
@@ -39,33 +30,20 @@ const getMarkdownString = (hover: Hover): string => {
   return hover.contents.value;
 }
 
-const Entry = ({ index, searchResult }: { index: number, searchResult: SearchResult }) => {
-  return <>
-    <Typography variant="subtitle1">Result {index}</Typography>
-    <Typography variant="subtitle2">Hovers</Typography>
-    <ReactMarkdown children={getMarkdownString(searchResult.hover.result)} />
-    <Typography variant="subtitle2">Definition</Typography>
-    <Typography>{JSON.stringify(searchResult.definition)}</Typography>
-    <ul>
-      {searchResult.defRanges.map((defRange, i) =>
-        <li key={i}>
-          <Typography>{JSON.stringify(defRange)}</Typography>
-        </li>
-      )}
-    </ul>
-    <Typography variant="subtitle2">Moniker</Typography>
-    <Typography>{JSON.stringify(searchResult.moniker)}</Typography>
-  </>
-};
-
 function App() {
   const endpoint = "http://localhost:8080/search"
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [query, setQuery] = React.useState('');
   const [searchResults, setSearchResults] = React.useState([] as SearchResult[]);
 
+  const { handleSubmit, control } = useForm();
+  const onSubmit = (data: { query: string }) => {
+    console.log(data);
+    setQuery(data.query);
+  };
+
   useEffect(() => {
-    if (searchTerm.length > 0) {
-      fetch(endpoint + "?q=" + searchTerm, { mode: 'cors' })
+    if (query.length > 0) {
+      fetch(endpoint + "?q=" + query, { mode: 'cors' })
         .then(response => response.json())
         .then(data => { console.log(data); return data; })
         .then(data => setSearchResults(data))
@@ -74,13 +52,41 @@ function App() {
           setSearchResults([])
         })
     }
-  }, [searchTerm]);
+  }, [query]);
 
   return (
     <Container sx={{ paddingTop: 2 }}>
-      <SearchBar onSearchTermChange={setSearchTerm} />
-      <Typography>{searchTerm.length > 0 ? `Searching for ${searchTerm}` : 'Please enter a search term'}</Typography>
-      <SearchResults searchResults={searchResults} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="query"
+          defaultValue=""
+          render={({ field }) => <Input {...field} />}
+        />
+        <input type="submit" />
+      </form>
+      <Typography>{query.length > 0 ? `Searching for ${query}` : 'Please enter a search term'}</Typography>
+      <Stack spacing={2}>
+        {searchResults.map((searchResult, index) =>
+          <Paper variant="outlined">
+            <Typography variant="subtitle1" sx={{ color: 'primary.main' }}>Result {index}</Typography>
+            <Typography variant="subtitle2" sx={{ color: 'secondary.main' }}>Hovers</Typography>
+            <ReactMarkdown className="markdown" children={getMarkdownString(searchResult.hover.result)} />
+            <Typography variant="subtitle2" sx={{ color: 'secondary.main' }}>Definition</Typography>
+            <Typography>{JSON.stringify(searchResult.definition, null, 2)}</Typography>
+            <ul>
+              {searchResult.defRanges.map((defRange, i) =>
+                <li key={i}>
+                  <Typography>{JSON.stringify(defRange, null, 2)}</Typography>
+                </li>
+              )}
+            </ul>
+            <Typography variant="subtitle2" sx={{ color: 'secondary.main' }}>Moniker</Typography>
+            <Typography>{JSON.stringify(searchResult.moniker, null, 2)}</Typography>
+          </Paper>
+          // <Entry key={i} index={i} searchResult={searchResult} />
+        )}
+      </Stack>
     </Container>
   );
 }
