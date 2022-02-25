603a4eb562023d45b8ff700c65a6de4f2ffd9997
Yuya Kono <takohati0821@gmail.com>
2022-02-25 15:41:34 +0900

    Add a simple test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Aeson (Value (Null))
+import qualified Data.Aeson as Aeson
+import Hovercraft
+import Language.LSP.Types
+import Relude
+import System.Directory.Extra (makeAbsolute)
+import System.FilePath
+import System.Process (callProcess)
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  (rootPath, uri) <- initialize
+  hspec $
+    describe "index-lsp" $ do
+      hovercraft <- runIO $ Aeson.decodeFileStrict "test/testcases/hello.test.json"
+      it "hello.test" $ hovercraft `shouldBe` Just (helloTestHovercraft rootPath uri)
+
+-- | Initialize the test suite.
+initialize :: IO (FilePath, Uri)
+initialize = do
+  -- Generate a hovercraft file for testcases/hello.test.
+  testcase <- makeAbsolute "test/testcases/hello.test"
+  callProcess "stack" ["exec", "--", "sisku", "index-lsp", testcase, "--output", testcase ++ ".json", "--config", "test/testcases/sisku_config.json"]
+  let rootPath = takeDirectory testcase
+  pure (rootPath, Uri {getUri = toText $ "file://" <> testcase})
+
+helloTestHovercraft :: FilePath -> Uri -> Hovercraft
+helloTestHovercraft rootPath uri =
+  Hovercraft
+    { _projectId = "com.github.takoeight0821.sisku.test",
+      _pages =
+        [ Page
+            { _entries =
+                [ Entry
+                    { _document = TextDocumentIdentifier {_uri = uri},
+                      _projectId = "com.github.takoeight0821.sisku.test",
+                      _hover =
+                        Hover
+                          { _contents = HoverContents $ MarkupContent {_kind = MkMarkdown, _value = "\n```test\nHello world\n```\n"},
+                            _range = Just $ mkRange 0 1 0 1
+                          },
+                      _definitions = [Definition {_uri = uri, _range = mkRange 0 0 0 0}],
+                      _moniker = Null,
+                      _rootPath = rootPath
+                    }
+                ]
+            }
+        ]
+    }
